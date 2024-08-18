df8c37a62a8c67b1fc26b05f51d99845c49a3b09
Prajwal K <prajwalkreddy17@gmail.com>
2024-08-18 14:43:12 +0530

    changes in test file

--- app/(root)/test/page.tsx
+++ app/(root)/test/page.tsx
@@ -1,50 +1,51 @@
-"use client"
+// "use client"
 
 
-import { UploadButton } from "@/utils/uploadthing";
-import { UploadDropzone } from "@/utils/uploadthing";
+// import { UploadButton } from "@/utils/uploadthing";
+// import { UploadDropzone } from "@/utils/uploadthing";
 
-export default function Test() {
-  return (<>
-    <h2>hello world </h2>
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+// export default function Test() {
+//   return (<>
+//     <h2>hello world </h2>
+//     <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
-      {/* <UploadButton
-        endpoint="imageUploader"
-        onClientUploadComplete={(res) => {
-          console.log("Files: ", res);
-          alert("Upload Completed");
-        }}
-        onUploadError={(error: Error) => {
-          alert(`ERROR! ${error.message}`);
-        }}
-      /> */}
-
-
-      <UploadDropzone
-        endpoint="imageUploader"
-        onClientUploadComplete={(res) => {
-          console.log("Files: ", res);
-          alert("Upload Completed");
-        }}
-        onUploadError={(error: Error) => {
-          alert(`ERROR! ${error.message}`);
-        }}
-      />
-
-    </main >
-  </>)
-}
+//       {/* <UploadButton
+//         endpoint="imageUploader"
+//         onClientUploadComplete={(res) => {
+//           console.log("Files: ", res);
+//           alert("Upload Completed");
+//         }}
+//         onUploadError={(error: Error) => {
+//           alert(`ERROR! ${error.message}`);
+//         }}
+//       /> */}
 
 
-// export default async function Page() {
-//   const users = await prisma.user.findMany();
+//       <UploadDropzone
+//         endpoint="imageUploader"
+//         onClientUploadComplete={(res) => {
+//           console.log("Files: ", res);
+//           alert("Upload Completed");
+//         }}
+//         onUploadError={(error: Error) => {
+//           alert(`ERROR! ${error.message}`);
+//         }}
+//       />
 
-//   return (
-//     <div>
-//       {users.map((user) => (
-//         <p key={user.id}>{user.name}</p>
-//       ))}
-//     </div>
-//   );
+//     </main >
+//   </>)
 // }
+
+import prisma from "@/lib/db";
+
+export default async function Page() {
+  const users = await prisma.user.findMany();
+
+  return (
+    <div>
+      {users.map((user) => (
+        <p key={user.id}>{user.name}</p>
+      ))}
+    </div>
+  );
+}
