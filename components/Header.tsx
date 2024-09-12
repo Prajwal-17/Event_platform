6bd0e00d10a42f7e2c4f45ac4a93c3055cf41a6c
Prajwal K <prajwalkreddy17@gmail.com>
2024-09-12 18:01:17 +0530

    route changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,24 +6,24 @@ export default function Header() {
 
   return (<>
     <header className="w-full  flex text-center justify-between px-24 py-5">
-      <div className="flex w-full justify-between items-center ">
-        <Link href="/" >
-          <Image
-            src="/logo.svg" width={128} height={38}
-            alt="Evently logo"
-          />
-        </Link>
-        <div className="p-2 font-[600] flex  gap-4 ">
-          <Link href='/' className="hover:text-[#4732d1]">Home</Link>
-          <Link href='/create-event' className="hover:text-[#4732d1]">Create Event</Link>
-          <Link href='my-profile' className="hover:text-[#4732d1]">My Profile</Link>
-        </div>
+      <nav className="flex w-full justify-between items-center ">
+          <Link href="/" >
+            <Image
+              src="/logo.svg" width={128} height={38}
+              alt="Evently logo"
+            />
+          </Link>
+          <div className="p-2 font-[600] flex  gap-4 ">
+            <Link href='/' className="hover:text-[#4732d1]">Home</Link>
+            <Link href='/events/create-event' className="hover:text-[#4732d1]">Create Event</Link>
+            <Link href='my-profile' className="hover:text-[#4732d1]">My Profile</Link>
+          </div>
 
-        <div>
-          <LoginAndLogout />
-        </div>
+          <div>
+            <LoginAndLogout />
+          </div>
 
-      </div>
+      </nav>
     </header>
     <hr />
   </>)
